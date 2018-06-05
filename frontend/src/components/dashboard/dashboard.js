0e0888e46890bcdf20fccca38409b7aea63a45be
cara ottmar <cara.ottmar@gmail.com>
2018-06-05 14:55:21 -0700

    front-end scaffold comoplete

---
+++ frontend/src/components/dashboard/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div className='dashboard'>
+        <h1> ::Dashboard:: </h1>
+        <h2> You can only see this if login successful! </h2>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
