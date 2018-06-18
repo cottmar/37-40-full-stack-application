532ee904ed1bc0cd5c5e844dd58bd9ee703052f8
cara ottmar <cara.ottmar@gmail.com>
2018-06-18 00:22:19 -0700

    updated readme

--- frontend/src/actions/client-picture.js
+++ frontend/src/actions/client-picture.js
@@ -1,10 +1,10 @@
 import superagent from 'superagent';
 import * as routes from '../routes';
 
-const set = pictures => ({ // eslint-disable-line
-  type: 'CLIENT_PICTURES_SET', // Name Convention: NAME_ACTION  or ACTION_NAME
-  payload: pictures,
-});
+// const set = pictures => ({ // eslint-disable-line
+//   type: 'CLIENT_PICTURES_SET', // Name Convention: NAME_ACTION  or ACTION_NAME
+//   payload: pictures,
+// });
 
 const create = picture => ({
   type: 'CLIENT_PICTURE_CREATE',
@@ -14,18 +14,18 @@ const create = picture => ({
 //-----------------------------------------------------------------
 // ASYNC
 //-----------------------------------------------------------------
-const createRequest = fileDescriptor => (store) => {
+const createRequest = photo => (store) => {
   // since pictures are a protected resource, I'll need a token
   const { token } = store.getState();
 
   return superagent.post(`${API_URL}${routes.PHOTOS_ROUTE}`)
     .set('Authorization', `Bearer ${token}`)
-    .field('description', fileDescriptor.description)
-    .attach('photo', fileDescriptor.picture)
+    .field('description', photo.description)
+    .attach('photo', photo.picture)
     .then((response) => {
       // Finish by updating the store
       return store.dispatch(create(response.body));
     });
 };
 
-export default { createRequest };
+export default { create, createRequest };
